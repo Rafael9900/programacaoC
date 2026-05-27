500baaf2684ef58b81257b198da741fc0a35bfd8
Rafael9900 <160249588+Rafael9900@users.noreply.github.com>
2026-05-27 15:13:29 -0300

    Update supertrunfo.c

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
 
+int main (){
+
+    char estado1[1];
+    char estado2[1];
+    char codigo1[2];
+    char codigo2[2];
+    char nome1[50];
+    char nome2[50];
+    int populacao1, populacao2, turistico1, turistico2;
+    float area1, area2;
+    float pib1, pib2;
+
+    //Primeira Carta
+    printf("Digite a letra do nome do primeiro estado: \n");
+    scanf("%s", &estado1);
+
+    printf("Digite o codigo do estado: \n");
+    scanf("%s", &codigo1);
+
+    printf("Digite o nome do primeiro estado: \n");
+    scanf("%s", &nome1);
+
+    printf("Digite a populucao estado: \n");
+    scanf("%d", &populacao1);
+
+    printf("Digite area estado: \n");
+    scanf("%f", &area1);
+
+    printf("PIB estado: \n");
+    scanf("%f", &pib1);
+
+    printf("Pontos turisticos estado: \n");
+    scanf("%d", &turistico1);
+    
+    //Segunda carta
+    printf("Digite a letra do nome do segundo estado: \n");
+    scanf("%s", &estado2);
+
+    printf("Digite o codigo do estado: \n");
+    scanf("%s", &codigo2);
+
+    printf("Digite o nome do segundo estado: \n");
+    scanf("%s", &nome2);
+
+    printf("Digite a populucao estado: \n");
+    scanf("%d", &populacao2);
+
+    printf("Digite area estado: \n");
+    scanf("%f", &area2);
+
+    printf("PIB estado: \n");
+    scanf("%f", &pib2);
+
+    printf("Pontos turisticos estado: \n");
+    scanf("%d", &turistico2);
+
+    
+}
